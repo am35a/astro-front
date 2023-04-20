33d8f05ccc19bb1ab146b65e9537accb99558ac8
Arthur A. Selimov <am35a.piston@gmail.com>
2023-04-20 07:41:03 +0300

    remove onMount for Date()

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,21 @@
 <script>
-    // import { onMount } from 'svelte'
-
     import Constellation from './lib/Constellation.svelte'
     import Stars from './lib/Stars.svelte'
     import Main from './lib/Main.svelte'
 
+    const theDate = new Date()
     let rotateDes
     let currentTimeObj = {
-        day: undefined,
-        month: undefined,
-        year: undefined
+        day: theDate.getDate(),
+        month: theDate.getMonth() + 1, // Jan is 0
+        year: theDate.getFullYear()
     }
-
-    // onMount(async () => {
-        const theDate = new Date()
-        currentTimeObj.day = theDate.getDate()
-        currentTimeObj.month = theDate.getMonth() + 1 // Jan is 0
-        currentTimeObj.year = theDate.getFullYear();
-    // })
 
     // console.log(currentTimeObj)
 </script>
 
 <Stars {rotateDes} />
 <Main>
-    <Constellation bind:rotateDes day={currentTimeObj.day} month={currentTimeObj.month} />
+    <Constellation bind:rotateDes {...currentTimeObj} />
 </Main>
 
