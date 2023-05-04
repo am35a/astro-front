78d9fc9427c200b682011d93a37c26ef0df05a6d
Arthur A. Selimov <am35a.piston@gmail.com>
2023-05-04 10:08:37 +0300

    added horo text array

--- src/lib/modal/Horoscope.svelte
+++ src/lib/modal/Horoscope.svelte
@@ -4,6 +4,33 @@
     let periodsArr = ['Today', 'Tomorrow', 'This week', 'Year']
     let period = 0
 
+    let horoscopeObj = [
+        [
+            'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt nam obcaecati temporibus consectetur pariatur reiciendis velit cum amet possimus soluta? Consequatur deserunt quaerat et tenetur velit ducimus eius consequuntur cumque!',
+            'Eos, pariatur! Ratione magnam et eius vero obcaecati doloremque delectus sapiente fuga! Pariatur hic provident sit vero dolorem, magnam obcaecati eaque. Debitis culpa quas amet fugiat natus. Architecto, sequi hic.',
+            'Sed veritatis voluptatum quae officia beatae perspiciatis non blanditiis minus maiores, quis alias, saepe explicabo nostrum optio earum necessitatibus consequatur omnis fugiat hic? A officiis laudantium pariatur iste consequatur corrupti!',
+            'Ipsam nihil fugit aliquid distinctio, esse quisquam explicabo ut. Officiis, nulla quis unde mollitia reiciendis expedita neque nobis sint non error. Aliquid doloribus ex ab quasi ea? Velit, neque laborum.'
+        ],
+        [
+            'Ratione tempore commodi voluptatem laudantium vitae quaerat ut quam, dolor, amet fuga iure. Magnam repellat ipsam molestiae dolorum maiores consequatur ipsa eaque incidunt! Sit exercitationem dolor, enim sequi et ipsa!',
+            'Iusto eligendi assumenda, officiis ex aperiam repellendus dolores nam facere ea sed, cumque culpa praesentium est ratione inventore! Laboriosam recusandae aliquid unde debitis dolorem deserunt quidem velit beatae magnam eaque!',
+            'Id vel quia dolores corporis veritatis repellendus accusamus ducimus! Blanditiis necessitatibus illo dignissimos alias odit voluptates laboriosam sint voluptas reprehenderit est consectetur, aperiam voluptatum, tempora a nesciunt vero et? Repellendus.',
+            'Asperiores minus ab beatae voluptatum. Sed perferendis similique dignissimos praesentium, itaque rerum. Praesentium modi eligendi quas eaque est commodi perferendis distinctio eius? Laboriosam vero dolore a consequatur unde accusantium odio?'
+        ],
+        [
+            'Natus ea, numquam expedita perspiciatis amet repellat maiores, blanditiis animi quibusdam in asperiores sit adipisci? Exercitationem ab ullam sit veritatis, aspernatur repellat nulla facere ipsa, quisquam, laudantium dolorem facilis quos.',
+            'Unde, beatae saepe ipsam incidunt voluptate exercitationem numquam! Est officia, ipsum beatae ab minima, dignissimos architecto esse in dolores tempore libero molestiae quis. Modi, excepturi dolorum. Incidunt amet perferendis voluptates.',
+            'Eos deserunt, obcaecati a vero quisquam esse libero corporis possimus impedit adipisci blanditiis alias magnam ipsam odit. Ipsa quas, facere esse voluptate repellendus ea? Iure laborum eos accusamus et mollitia.',
+            'Voluptate, ea minus provident deleniti sequi rerum eaque facere veritatis aspernatur pariatur amet alias placeat commodi vel omnis dolorum beatae iste. Repellendus, molestiae. Id doloremque quis nemo nisi quod repellendus.'
+        ],
+        [
+            'Eos, pariatur! Ratione magnam et eius vero obcaecati doloremque delectus sapiente fuga! Pariatur hic provident sit vero dolorem, magnam obcaecati eaque. Debitis culpa quas amet fugiat natus. Architecto, sequi hic.',
+            'Id vel quia dolores corporis veritatis repellendus accusamus ducimus! Blanditiis necessitatibus illo dignissimos alias odit voluptates laboriosam sint voluptas reprehenderit est consectetur, aperiam voluptatum, tempora a nesciunt vero et? Repellendus.',
+            'Iusto eligendi assumenda, officiis ex aperiam repellendus dolores nam facere ea sed, cumque culpa praesentium est ratione inventore! Laboriosam recusandae aliquid unde debitis dolorem deserunt quidem velit beatae magnam eaque!',
+            'Natus ea, numquam expedita perspiciatis amet repellat maiores, blanditiis animi quibusdam in asperiores sit adipisci? Exercitationem ab ullam sit veritatis, aspernatur repellat nulla facere ipsa, quisquam, laudantium dolorem facilis quos.'
+        ]
+    ]
+
     function modalLock(event) {
         // console.log(event.target.scrollTop)
 
@@ -32,7 +59,7 @@
 <section>
     <div class="title">Horoscope</div>
     <div class="button-group">
-        {#each periodsArr as item, i }
+        {#each periodsArr as item, i}
             <button
                 on:click={() => period = i}
                 disabled={period === i}
@@ -40,16 +67,14 @@
             >{item}</button>
         {/each}
     </div>
+
     <div
         class="body"
         on:scroll={() => modalLock(event)}
     >
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet recusandae iste distinctio molestiae. Voluptates suscipit numquam doloribus veniam, magni modi nisi recusandae, corporis et laborum reprehenderit exercitationem iure corrupti labore.</p>
-        <p>Sapiente aperiam facilis hic placeat porro dolorum in nulla dicta beatae corporis repellat deserunt asperiores, sint fugiat repudiandae facere ex nesciunt sunt. Totam earum pariatur, ipsa tempore temporibus aperiam necessitatibus.</p>
-        <p>Incidunt natus ex, ad dolore facilis quia voluptatem hic asperiores necessitatibus maiores distinctio deserunt sequi ut autem. Neque animi in error itaque, nisi voluptatum ab ut, cumque, eligendi deserunt tempora!</p>
-        <p>Est deleniti commodi deserunt nisi repellendus quidem iusto veritatis, minus quis libero possimus eius dolore eligendi, cumque quae repudiandae ad dolor autem quasi eaque accusantium labore? Atque maxime quo modi?</p>
-        <p>Sunt non distinctio vel beatae illum voluptatibus architecto excepturi, nobis magnam obcaecati quos eum, at exercitationem quasi velit neque impedit enim perspiciatis! Eos harum provident a omnis optio rem? Id.</p>
-        <p>Doloremque quam esse repudiandae delectus quia ipsa dolore mollitia rerum, voluptatum sequi odit quos nihil sint facilis eligendi earum voluptatem numquam aliquid quod minus ex commodi ea nam libero? Tenetur?</p>
+        {#each horoscopeObj[period] as text}
+            <p>{text}</p>
+        {/each}
     </div>
 </section>
 
