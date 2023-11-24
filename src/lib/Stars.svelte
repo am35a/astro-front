2dab6baa268ca7c603bc2fd8f9fc4a63ee95616a
Arthur A. Selimov <am35a.piston@gmail.com>
2023-11-24 17:06:46 +0300

    added stars slide by Y axys

--- src/lib/Stars.svelte
+++ src/lib/Stars.svelte
@@ -1,7 +1,17 @@
 <script>
+    import { route } from '../store/route'
     import skyPattern from '/img/sky-pattern.svg'
 
     export let rotateDes = 0
+
+    var translateY = 0
+    $: switch ($route.modal) {
+        case 'close':
+            translateY = 1.5
+        break
+        default:
+            translateY = 0
+    }
 </script>
 
 <!-- svelte-ignore a11y-missing-attribute -->
@@ -9,18 +19,21 @@
     class="layer-1"
     src={skyPattern}
     style:--rotate-deg={`${-90 - rotateDes * 3}deg`}
+    style:--translate-y={`${translateY  * 3}vh`}
 >
 <!-- svelte-ignore a11y-missing-attribute -->
 <img
     class="layer-2"
     src={skyPattern}
     style:--rotate-deg={`${-45 - rotateDes * 1.5}deg`}
+    style:--translate-y={`${translateY  * 1.5}vh`}
 >
 <!-- svelte-ignore a11y-missing-attribute -->
 <img
     class="layer-3"
     src={skyPattern}
     style:--rotate-deg={`${-rotateDes}deg`}
+    style:--translate-y={`${translateY}vh`}
 >
 
 <style>
@@ -29,7 +42,7 @@
         grid-row: 1/-1;
 
         height: 200vh;
-        transform: translateX(calc(-50% + 50vw)) rotate(var(--rotate-deg));
+        transform: translateX(calc(-50% + 50vw)) translateY(var(--translate-y)) rotate(var(--rotate-deg));
         transition: transform .375s linear;
         will-change: transform;
     }
