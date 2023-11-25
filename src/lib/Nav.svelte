5bbd63af68f2f9ed96289ee0f074c1deeb96e01f
Arthur A. Selimov <am35a.piston@gmail.com>
2023-11-25 21:45:41 +0300

    fixed icon path union of about icon

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -35,10 +35,18 @@
             class="circle"
             disabled={$route.modalMotion}
         >
-            <svg xmlns="http://www.w3.org/2000/svg" width="2rem" height="2rem" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <!-- <svg xmlns="http://www.w3.org/2000/svg" width="2rem" height="2rem" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                 <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" />
                 <path d="M12 9h.01" />
                 <path d="M11 12h1v4h1" />
+            </svg> -->
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="2rem" height="2rem"
+                viewBox="-4 -4 56 56"
+            >
+                <path d="M43 26C46.6873 32.8916 32.9733 52.6875 13 42.0001C30.6835 38.4956 39.4998 24.6832 43 26Z" fill="currentColor" />
+                <path d="M 3 24 C 3 13.152 12.402 3 24 3 C 34.473 3 41.882 10.54 37.007 20.658 C 42.396 15.189 45.074 10.173 43.385 7.761 C 42.852 7 41.927 6.559 40.692 6.412 C 43.741 6.138 46.026 6.73 47.129 8.305 C 50.297 12.829 42.51 23.747 29.736 32.691 C 16.962 41.636 4.039 45.219 0.871 40.695 C -0.216 39.142 -0.013 36.836 1.24 34.115 C 0.982 35.285 1.089 36.272 1.608 37.013 C 2.685 38.551 5.364 38.783 8.975 37.921 C 5.096 35.647 3.113 30.202 3 24 Z" fill="currentColor" />
             </svg>
         </Button>
         <!--
